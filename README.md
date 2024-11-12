384c43aa026478f16f974b97764f03bf758e08c5
lindsayh17 <lhall11@uvm.edu>
2024-11-12 12:54:53 -0500

    DO NOT RUN (breaks computer)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LindsayAndHannah
 This is for when Lindsay and Hannah have to be CS majors together
+
+I guess we should use snake case for snake game
--- snake.py
+++ snake.py
@@ -1,2 +1,47 @@
+# importing modules
 import pygame
+import time
 import random
+
+# Window size
+window_x = 720
+window_y = 480
+
+# Initializing pygame
+pygame.init()
+
+# Initialize game window
+pygame.display.set_caption('Snake Game')
+game_window = pygame.display.set_mode((window_x, window_y))
+
+# initial score
+score = 0
+
+#initial running status
+running = True
+
+# initialize snake
+snake_x = 100
+snake_y = 100
+
+# Main Function
+while running:
+
+    # keys
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        snake_y -= 5
+    if keys[pygame.K_DOWN]:
+        snake_y += 5
+    if keys[pygame.K_LEFT]:
+        snake_x -= 5
+    if keys[pygame.K_RIGHT]:
+        snake_x += 5
+
+    square = pygame.Rect(snake_x, snake_y, 5, 5)
+
+    if snake_x > window_x or snake_y > window_y:
+        running = False
+
+    # Refresh game screen
+    pygame.display.flip()
