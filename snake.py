9027dc426e2462a0bf3c8460a4b37119eb973bed
lindsayh17 <lhall11@uvm.edu>
2024-11-12 16:32:54 -0500

    snake moves

--- snake.py
+++ snake.py
@@ -17,10 +17,14 @@ game_window = pygame.display.set_mode((window_x, window_y))
 # initial score
 score = 0
 
+#clock
+clock = pygame.time.Clock()
+
 #initial running status
 running = True
 
 # initialize snake
+direction = "RIGHT"
 snake_x = 100
 snake_y = 100
 
@@ -30,19 +34,31 @@ while running:
     # keys
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
-        snake_y -= 5
+        direction = "UP"
     if keys[pygame.K_DOWN]:
-        snake_y += 5
+        direction = "DOWN"
     if keys[pygame.K_LEFT]:
-        snake_x -= 5
+        direction = "LEFT"
     if keys[pygame.K_RIGHT]:
+        direction = "RIGHT"
+
+    if direction == "UP":
+        snake_y -= 5
+    if direction == "DOWN":
+        snake_y += 5
+    if direction == "LEFT":
+        snake_x -= 5
+    if direction == "RIGHT":
         snake_x += 5
 
     snake = pygame.Rect(snake_x, snake_y, 5, 5)
-    pygame.draw.rect(game_window, (0,0,0), snake)
+    pygame.draw.rect(game_window, (255,0,0), snake)
 
-    if snake_x > window_x or snake_y > window_y:
-        running = False
+    # TODO: get rid of snake body or let it get longer
+
+    # TODO: display score
+
+    # TODO: set up bounds for walls (and maybe snake itself - store in an array?)
 
     for event in pygame.event.get():
         # Check if the user closes the window
@@ -51,5 +67,6 @@ while running:
 
     # Refresh game screen
     pygame.display.flip()
+    clock.tick(30)
 
 pygame.quit()
